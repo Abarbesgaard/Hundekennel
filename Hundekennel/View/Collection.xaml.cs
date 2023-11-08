f456301e3bf6acac8c574962827a453c9c501c49
Abarbesgaard <Abarbesgaard@gmail.com>
2023-11-08 11:07:01 +0100

    Knap virker

--- Hundekennel/View/Collection.xaml.cs
+++ Hundekennel/View/Collection.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hundekennel.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace Hundekennel.View
     /// </summary>
     public partial class Collection : UserControl
     {
+       
         public Collection()
         {
             InitializeComponent();
+               
         }
     }
 }
--- Hundekennel/obj/Debug/net7.0-windows/View/HomeView.g.i.cs
+++ Hundekennel/obj/Debug/net7.0-windows/View/HomeView.g.i.cs
@@ -53,7 +53,7 @@ namespace Hundekennel.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Hundekennel;V1.0.0.0;component/view/homeview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Hundekennel;component/view/homeview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\HomeView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
